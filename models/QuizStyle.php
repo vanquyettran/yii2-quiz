e79144c4337347deb052a316b8e7976da9b85524
vanquyettran <tvq16091993tvq>
2017-09-09 01:14:09 +0700

    placeholder

--- models/QuizStyle.php
+++ models/QuizStyle.php
@@ -6,5 +6,19 @@ use Yii;
 
 class QuizStyle extends \common\modules\quiz\baseModels\QuizStyle
 {
+    public static function modelConfig()
+    {
+        $modelConfig = parent::modelConfig();
 
+        foreach ($modelConfig['attrs'] as &$attr) {
+            switch ($attr['name']) {
+                case 'z_index':
+                    $attr['placeholder'] = '6 --- the stack order of the layer';
+                    break;
+
+            }
+        }
+
+        return $modelConfig;
+    }
 }
