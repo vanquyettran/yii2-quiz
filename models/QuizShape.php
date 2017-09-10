f2ac1820c3086a52afc1d2ca58a0230cda761fd3
vanquyettran <tvq16091993tvq>
2017-09-10 14:16:40 +0700

    quiz

--- models/QuizShape.php
+++ models/QuizShape.php
@@ -15,11 +15,11 @@ class QuizShape extends \common\modules\quiz\baseModels\QuizShape
                 $attr['type'] = 'TextArea';
                 $attr['placeholder'] =
                     "You can embed data to text:\n"
-                    . "{{@r.params.var_name}} will be replaced by value of param `var_name`\n"
-                    . "{{@r.inputs.var_name.value}} will be replaced by value of input `var_name`\n"
-                    . "{{@r.characters.var_name.name}} will be replaced by name of character `var_name`\n"
-                    . "{{@statistics.score}} will be replaced by current score of all inputs\n"
-                    . "{{@elapsedTime.total}}, {{@elapsedTime.allQAs}} and {{@elapsedTime.closedQAs}} will be replaced by total, all questions answering and closed questions answering elapsed time\n"
+                    . "{{@r.params.var_name}} => value of param `var_name`\n"
+                    . "{{@r.inputs.var_name.value}} => value of input `var_name`\n"
+                    . "{{@r.characters.var_name.name}} => name of character `var_name`\n"
+                    . "{{@statistics.score}} => current score of all inputs\n"
+                    . "{{@elapsedTime.total}}, {{@elapsedTime.allQAs}} and {{@elapsedTime.closedQAs}} => total, all questions answering and closed questions answering elapsed time\n"
                 ;
             }
         }
