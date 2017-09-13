fc8e862c1019d2b6db6e25f7d5f05fe7c8d4ec55
vanquyettran <tvq16091993tvq>
2017-09-14 01:35:45 +0700

    quiz

--- views/default/editor.php
+++ views/default/editor.php
@@ -55,7 +55,7 @@ if (Yii::$app->request->get('use_local_asset') == 1) {
                     errors: {
                         HTTP_ERROR: 'Status: ' + this.status
 //                        + ', Status Text: ' + this.statusText
-                        + ', Response Text' + this.responseText
+//                        + ', Response Text' + this.responseText
                     },
                     success: false
                 });
